923215cb3abaa9374d5aa4beec354961458cbe4f
Talibiq123 <talibsaeed90@gmail.com>
2025-01-01 18:59:16 +0530

    Union

--- 01. SQL/006. Union.sql
+++ 01. SQL/006. Union.sql
@@ -7,3 +7,46 @@ FROM employee_demographics
 UNION
 SELECT first_name, last_name
 FROM employee_salary;
+
+SELECT age, gender
+FROM employee_demographics
+UNION DISTINCT
+SELECT first_name, last_name
+FROM employee_salary;
+
+SELECT age, gender
+FROM employee_demographics
+UNION ALL
+SELECT first_name, last_name
+FROM employee_salary;
+
+
+SELECT first_name, last_name, 'Old' as Label
+FROM employee_demographics
+WHERE age>50
+UNION
+SELECT first_name, last_name, 'Highly Paid Employee' as Label
+FROM employee_salary
+WHERE salary>70000;
+
+SELECT first_name, last_name, 'Old Man' as Label
+FROM employee_demographics
+WHERE age>40 AND gender = 'Male'
+UNION
+SELECT first_name, last_name, 'Old Lady' as Label
+FROM employee_demographics
+WHERE age>40 AND gender = 'Female'
+UNION
+SELECT first_name, last_name, 'Highly Paid Employee' as Label
+FROM employee_salary
+WHERE salary>70000;
+
+
+
+
+
+
+
+
+
+
